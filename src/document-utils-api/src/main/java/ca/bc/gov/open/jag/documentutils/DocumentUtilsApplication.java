72800429609e828b04c1f94d991b66ed069c97af
alex <alexandre.joyeux@nttdata.com>
2020-11-17 15:15:22 -0800

    fix issue with component scan

--- src/document-utils-api/src/main/java/ca/bc/gov/open/jag/documentutils/DocumentUtilsApplication.java
+++ src/document-utils-api/src/main/java/ca/bc/gov/open/jag/documentutils/DocumentUtilsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = "ca.bc.gov.open.pssg.docmerge")
+@SpringBootApplication
 @EnableConfigurationProperties(ConfigProperties.class)
 public class DocumentUtilsApplication {
 
