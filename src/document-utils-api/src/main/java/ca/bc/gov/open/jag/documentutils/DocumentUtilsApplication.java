aec91f673acf2067c3d0b8f4b9e043acc4575b0d
alex <alexandre.joyeux@nttdata.com>
2020-11-18 13:55:11 -0800

    updating configuration

--- src/document-utils-api/src/main/java/ca/bc/gov/open/jag/documentutils/DocumentUtilsApplication.java
+++ src/document-utils-api/src/main/java/ca/bc/gov/open/jag/documentutils/DocumentUtilsApplication.java
@@ -1,12 +1,9 @@
 package ca.bc.gov.open.jag.documentutils;
 
-import ca.bc.gov.open.jag.documentutils.config.ConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ConfigProperties.class)
 public class DocumentUtilsApplication {
 
 	public static void main(String[] args) {
---
+++ src/document-utils-api/src/main/java/ca/bc/gov/open/jag/documentutils/adobe/AemProperties.java
@@ -0,0 +1,35 @@
+package ca.bc.gov.open.jag.documentutils.adobe;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "aem")
+public class AemProperties {
+
+    private String endpoint;
+    private String username;
+    private String password;
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
